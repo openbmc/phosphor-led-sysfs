5dc1049e8244daeb5c17d527dce6f5957ee78e5b
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2016-11-30 15:52:15 +0530

    Add methods to trigger action on LED
    Implements the ON, OFF and BLINK operations on a given LED.
    
    Change-Id: I74b5ac01d8e76961999a2673d52d73296f5603d7
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- physical.hpp
+++ physical.hpp
@@ -2,12 +2,44 @@
 
 #include <map>
 #include <set>
+#include <fstream>
 #include <sdbusplus/server/object.hpp>
 #include "xyz/openbmc_project/Led/Physical/server.hpp"
+
 namespace phosphor
 {
 namespace led
 {
+/** @brief Acts as input and output file for the current LED power state.
+ *   Write "0" to trigger a LED OFF operation.
+ *   Write "255" to trigger a LED ON operation.
+ *   To know the current power state of the LED, a read on this
+ *   would give either 0 or 255 indicating if the LED is currently
+ *   Off / On AT THAT moment.
+ *   Example: /sys/class/leds/myled/brightness
+ */
+constexpr auto BRIGHTNESS = "/brightness";
+
+/** @brief Write "timer" to this file telling the driver that
+ *   the intended operation is BLINK. When the value "timer" is written to
+ *   the file, 2 more files get auto generated and are named "delay_on" and
+ *   "delay_off"
+ *   To move away from blinking, write "none"
+ *   Example:  /sys/class/leds/myled/trigger
+ */
+constexpr auto BLINKCTRL = "/trigger";
+
+/** @brief write number of milliseconds that the LED needs to be ON
+ *   while blinking. Default is 500 by the driver.
+ *   Example:  /sys/class/leds/myled/delay_on
+ */
+constexpr auto DELAYON = "/delay_on";
+
+/** @brief Write number of milliseconds that the LED needs to be OFF
+ *   while blinking. Default is 500 by the driver.
+ *   Example:  /sys/class/leds/myled/delay_off
+ */
+constexpr auto DELAYOFF = "/delay_off";
 
 /** @class Physical
  *  @brief Responsible for applying actions on a particular physical LED
@@ -72,13 +104,41 @@ private:
      */
     Action driveLED(void);
 
-    /** @brief Generic file reader.
-     *    This is mostly used to read the 'brightness' file for a given LED
-     *    to know what is the current state of LED.
+    /** @brief Sets the LED to either ON or OFF state
      *
-     *  @param[in] fileName - Name of file to be read
-     *  @return             - File content
+     *  @return Current action or exception thrown
      */
+    Action stableStateOperation();
+
+    /** @brief Sets the LED to BLINKING
+     *
+     *  @return Current Action or exception thrown
+     */
+    Action blinkOperation();
+
+    /** @brief Generic file writer.
+     *   There are files like "brightness", "trigger" , "delay_on" and
+     *   "delay_off" that will tell what the LED driver needs to do.
+     *
+     * @param[in] filename - Name of file to be written
+     * @param[in] data     - Data to be written to
+     * @return             - 0 or exception.
+     */
+    template <typename T>
+    auto write(const std::string& fileName, T&& data)
+    {
+        if(std::ifstream(fileName))
+        {
+            std::ofstream file(fileName, std::ios::out);
+            file << data;
+            file.close();
+            return 0;
+        }
+        else
+        {
+            throw std::runtime_error(fileName  + " Not Present");
+        }
+    }
 };
 
 } // namespace led
