d48ad00e87c48382005c7a2400fec238375afd91
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2016-11-30 12:21:25 +0530

    Use generated bindings for physical led controller
    Extends the generated interface and provides skeleton implementation of led
    controller.
    
    Change-Id: I13485f39755213b8a7324e526d6335f3ee0a2b67
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ physical.cpp
@@ -0,0 +1,40 @@
+/**
+ * Copyright © 2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "physical.hpp"
+namespace phosphor
+{
+namespace led
+{
+
+/** @brief Initialize the bus and announce services */
+Physical::Physical(
+    sdbusplus::bus::bus& bus,
+    const std::string& objPath,
+    const std::string& ledName,
+    const std::string& ledPath) :
+
+    sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Led::server::Physical>(
+        bus, objPath.c_str()),
+    name(ledName),
+    path(ledPath)
+{
+    // Nothing to do here
+}
+
+} // namespace led
+} // namespace phosphor
---
+++ physical.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include "xyz/openbmc_project/Led/Physical/server.hpp"
+namespace phosphor
+{
+namespace led
+{
+
+/** @class Physical
+ *  @brief Responsible for applying actions on a particular physical LED
+ */
+class Physical : public sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Led::server::Physical>
+{
+    public:
+        Physical() = delete;
+        ~Physical() = default;
+        Physical(const Physical&) = delete;
+        Physical& operator=(const Physical&) = delete;
+        Physical(Physical&&) = default;
+        Physical& operator=(Physical&&) = default;
+
+        /** @brief Constructs LED object
+         *
+         * @param[in] bus       - system dbus handler
+         * @param[in] objPath   - The Dbus path that hosts physical LED
+         * @param[in] ledName   - The name of the LED like "EnclosureFault"
+         * @param[in] ledPath   - sysfs path where this LED is exported
+         */
+        Physical(sdbusplus::bus::bus& bus,
+                const std::string& objPath,
+                const std::string& ledName,
+                const std::string& ledPath);
+
+    private:
+        /** @brief Name of the LED, like "EnclosureFault" */
+        std::string name;
+
+        /** @brief File system location where this LED is exposed
+         *   Typically /sys/class/leds/
+         */
+        std::string path;
+};
+
+} // namespace led
+} // namespace phosphor
