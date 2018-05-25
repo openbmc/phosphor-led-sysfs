6de791da895617ccae425802732528579a46eb25
Andrew Jeffery <andrew@aj.id.au>
2018-05-25 13:04:39 +0930

    test: physical: Cover trigger:none, brigtness:asserted branch
    The test causes execution to take the following path in
    setInitialState(), previously outlined in 264d909d3dc9 ("test: Add tests
    for Physical class") as being missed:
    
        auto brightness = led.getBrightness();
        if (brightness == ASSERT)
        {
            // LED is in Solid ON
            sdbusplus::xyz::openbmc_project::Led::server ::Physical::state(
                Action::On);
        }
    
    This brings the line coverage to 97.6% (function coverage remains
    unchanged by this patch).
    
    Change-Id: I7a04fcd97819559575e69d267c62f16195495010
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- test/physical.cpp
+++ test/physical.cpp
@@ -115,3 +115,13 @@ TEST(Physical, blink)
     phosphor::led::Physical phy(bus, LED_OBJ, led);
     phy.state(Action::Blink);
 }
+
+TEST(Physical, ctor_none_trigger_asserted_brightness)
+{
+    sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
+    NiceMock<MockLed> led;
+    EXPECT_CALL(led, getTrigger()).WillRepeatedly(Return("none"));
+    constexpr auto val = phosphor::led::ASSERT;
+    EXPECT_CALL(led, getBrightness()).WillRepeatedly(Return(val));
+    phosphor::led::Physical phy(bus, LED_OBJ, led);
+}
