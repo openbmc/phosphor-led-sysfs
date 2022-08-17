97a036f5e7a9946cd3a61e6ab28213c04fb04ec1
Jayashree Dhanapal <jayashree-d@hcl.com>
2022-08-17 17:34:32 +0530

    Physical LED support for multihost.
    The existing phosphor-led-sysfs design exposes one service per LED
    configuration in device tree. Hence, multiple services will be
    created for multiple GPIO pins configured for LEDs.
    
    There are cases where multiple LEDs were configured in the device tree
    for each host and needs to be paired as a group of LEDs for the
    specified host in the multi host platform.
    
    For example, Power LED and System Identification LED combines into a
    single bicolor blue-yellow LED per host. A total of 4 × LEDs will be
    placed along the front edge of the board in a grid. The grid will be
    2 × rows of 2 × LEDs to match the layout of the card slots.
    
    Depending on the status of each host, blue or yellow LED needs to be
    blink, OFF or ON and other LEDs needs to be in OFF state. Therefore,
    bi-color led needs to be paired as a group and exposed in the userspace.
    
    Based on the current design in phopshor-led-sysfs application, pairing
    groups will be difficult, since it exposes one service per LED. To
    abstract this method and also to create LEDs under a single service, a
    new application is proposed.
    
    Tested : Tested and verfied in Facebook YosemiteV2 platform.
    
    Signed-off-by: Jayashree Dhanapal <jayashree-d@hcl.com>
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: I50e58126413514f7a64153f425629142fa5af6d4

--- controller.cpp
+++ controller.cpp
@@ -14,23 +14,20 @@
  * limitations under the License.
  */
 
-#include "argument.hpp"
 #include "physical.hpp"
 #include "sysfs.hpp"
 
 #include <boost/algorithm/string.hpp>
+#include <phosphor-logging/lg2.hpp>
 
 #include <algorithm>
 #include <iostream>
 #include <string>
 
-static void ExitWithError(const char* err, char** argv)
-{
-    phosphor::led::ArgumentParser::usage(argv);
-    std::cerr << std::endl;
-    std::cerr << "ERROR: " << err << std::endl;
-    exit(-1);
-}
+static constexpr auto busName = "xyz.openbmc_project.LED.Controller";
+static constexpr auto objectPath = "/xyz/openbmc_project/led/physical";
+static constexpr auto rootPath = "/xyz/openbmc_project/led";
+static constexpr auto devPath = "/sys/class/leds/";
 
 struct LedDescr
 {
@@ -79,73 +76,17 @@ std::string getDbusName(const LedDescr& ledDescr)
     return boost::join(words, "_");
 }
 
-int main(int argc, char** argv)
+int main()
 {
-    namespace fs = std::filesystem;
-    static constexpr auto BUSNAME = "xyz.openbmc_project.LED.Controller";
-    static constexpr auto OBJPATH = "/xyz/openbmc_project/led/physical";
-    static constexpr auto DEVPATH = "/sys/class/leds/";
-
-    // Read arguments.
-    auto options = phosphor::led::ArgumentParser(argc, argv);
-
-    // Parse out Path argument.
-    auto path = std::move((options)["path"]);
-    if (path == phosphor::led::ArgumentParser::empty_string)
-    {
-        ExitWithError("Path not specified.", argv);
-    }
-
-    // If the LED has a hyphen in the name like: "one-two", then it gets passed
-    // as /one/two/ as opposed to /one-two to the service file. There is a
-    // change needed in systemd to solve this issue and hence putting in this
-    // work-around.
-
-    // Since this application always gets invoked as part of a udev rule,
-    // it is always guaranteed to get /sys/class/leds/one/two
-    // and we can go beyond leds/ to get the actual LED name.
-    // Refer: systemd/systemd#5072
-
-    // On an error, this throws an exception and terminates.
-    auto name = path.substr(strlen(DEVPATH));
-
-    // LED names may have a hyphen and that would be an issue for
-    // dbus paths and hence need to convert them to underscores.
-    std::replace(name.begin(), name.end(), '/', '-');
-    path = DEVPATH + name;
-
-    // Convert to lowercase just in case some are not and that
-    // we follow lowercase all over
-    std::transform(name.begin(), name.end(), name.begin(), ::tolower);
-
-    // LED names may have a hyphen and that would be an issue for
-    // dbus paths and hence need to convert them to underscores.
-    std::replace(name.begin(), name.end(), '-', '_');
-
-    // Convert LED name in sysfs into DBus name
-    LedDescr ledDescr;
-    getLedDescr(name, ledDescr);
-    name = getDbusName(ledDescr);
-
-    // Unique bus name representing a single LED.
-    auto busName = std::string(BUSNAME) + '.' + name;
-    auto objPath = std::string(OBJPATH) + '/' + name;
-
-    // Get a handle to system dbus.
+    // Get a handle to system dbus
     auto bus = sdbusplus::bus::new_default();
 
-    // Add systemd object manager.
-    sdbusplus::server::manager_t(bus, objPath.c_str());
+    // Add the ObjectManager interface
+    sdbusplus::server::manager::manager objManager(bus, rootPath);
 
-    // Create the Physical LED objects for directing actions.
-    // Need to save this else sdbusplus destructor will wipe this off.
-    phosphor::led::SysfsLed sled{fs::path(path)};
-    phosphor::led::Physical led(bus, objPath, sled, ledDescr.color);
+    // Request service bus name
+    bus.request_name(busName);
 
-    /** @brief Claim the bus */
-    bus.request_name(busName.c_str());
-
-    /** @brief Wait for client requests */
     while (true)
     {
         // Handle dbus message / signals discarding unhandled
