37741dbd41747b3c87ad84d8076fcc6d87533625
Jayashree Dhanapal <jayashree-d@hcl.com>
2022-08-17 17:34:32 +0530

    Physical LEDs to display dbus path under single service
    The existing phosphor-led-sysfs design exposes one service per LED
    configuration in device tree. Hence, multiple services will be
    created for multiple GPIO pins configured for LEDs.
    
    There are cases where multiple LEDs were configured in the device tree
    for each host and needs to be paired as a group of LEDs for the
    specified host in the multi host platform.
    
    For example, Power LED and System Identification LED combines into a
    single bicolor blue-yellow LED per host. A total of 4 × LEDs will be
    placed along the front edge of the board in a grid. The grid will be
    2 × rows of 2 × LEDs to match the layout of the card slots.
    
    Depending on the status of each host, blue or yellow LED needs to be
    blink, OFF or ON and other LEDs needs to be in OFF state. Therefore,
    bi-color led needs to be paired as a group and exposed in the userspace.
    
    Based on the current design in phosphor-led-sysfs application, pairing
    groups will be difficult, since it exposes one service per LED. To
    abstract this method and also to create LEDs under a single service, a
    new application is proposed.
    
    Tested : Tested and verified by displaying LEDs dbus path under
    single service in Facebook YosemiteV2 platform.
    
    Signed-off-by: Jayashree Dhanapal <jayashree-d@hcl.com>
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: I50e58126413514f7a64153f425629142fa5af6d4

--- test/physical.cpp
+++ test/physical.cpp
@@ -74,9 +74,9 @@ TEST(Physical, ctor_none_trigger)
 {
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
     /* NiceMock ignores calls to methods with no expectations defined */
-    NiceMock<MockLed> led;
-    ON_CALL(led, getTrigger()).WillByDefault(Return("none"));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    ON_CALL(*led, getTrigger()).WillByDefault(Return("none"));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     EXPECT_EQ(phy.state(), Action::Off);
 }
 
@@ -84,11 +84,11 @@ TEST(Physical, ctor_maxbrightness_and_brightness_read_127)
 {
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
     /* NiceMock ignores calls to methods with no expectations defined */
-    NiceMock<MockLed> led;
-    EXPECT_CALL(led, getTrigger()).WillRepeatedly(Return("none"));
-    EXPECT_CALL(led, getBrightness()).WillOnce(Return(127));
-    EXPECT_CALL(led, getMaxBrightness()).WillOnce(Return(127));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    EXPECT_CALL(*led, getTrigger()).WillRepeatedly(Return("none"));
+    EXPECT_CALL(*led, getBrightness()).WillOnce(Return(127));
+    EXPECT_CALL(*led, getMaxBrightness()).WillOnce(Return(127));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     EXPECT_EQ(phy.state(), Action::On);
 }
 
@@ -96,11 +96,11 @@ TEST(Physical, ctor_maxbrightness_and_brightness_read_0)
 {
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
     /* NiceMock ignores calls to methods with no expectations defined */
-    NiceMock<MockLed> led;
-    EXPECT_CALL(led, getTrigger()).WillRepeatedly(Return("none"));
-    EXPECT_CALL(led, getBrightness()).WillOnce(Return(0));
-    EXPECT_CALL(led, getMaxBrightness()).WillOnce(Return(0));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    EXPECT_CALL(*led, getTrigger()).WillRepeatedly(Return("none"));
+    EXPECT_CALL(*led, getBrightness()).WillOnce(Return(0));
+    EXPECT_CALL(*led, getMaxBrightness()).WillOnce(Return(0));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     EXPECT_EQ(phy.state(), Action::Off);
 }
 
@@ -108,11 +108,11 @@ TEST(Physical, ctor_only_maxbrightness_read_127)
 {
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
     /* NiceMock ignores calls to methods with no expectations defined */
-    NiceMock<MockLed> led;
-    EXPECT_CALL(led, getTrigger()).WillRepeatedly(Return("none"));
-    EXPECT_CALL(led, getBrightness()).WillOnce(Return(0));
-    EXPECT_CALL(led, getMaxBrightness()).WillOnce(Return(127));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    EXPECT_CALL(*led, getTrigger()).WillRepeatedly(Return("none"));
+    EXPECT_CALL(*led, getBrightness()).WillOnce(Return(0));
+    EXPECT_CALL(*led, getMaxBrightness()).WillOnce(Return(127));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     EXPECT_EQ(phy.state(), Action::Off);
 }
 
@@ -120,34 +120,35 @@ TEST(Physical, ctor_only_brightness_read_127)
 {
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
     /* NiceMock ignores calls to methods with no expectations defined */
-    NiceMock<MockLed> led;
-    EXPECT_CALL(led, getTrigger()).WillRepeatedly(Return("none"));
-    EXPECT_CALL(led, getBrightness()).WillOnce(Return(127));
-    EXPECT_CALL(led, getMaxBrightness()).WillOnce(Return(0));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    EXPECT_CALL(*led, getTrigger()).WillRepeatedly(Return("none"));
+    EXPECT_CALL(*led, getBrightness()).WillOnce(Return(127));
+    EXPECT_CALL(*led, getMaxBrightness()).WillOnce(Return(0));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     EXPECT_EQ(phy.state(), Action::Off);
 }
 
 TEST(Physical, ctor_timer_trigger)
 {
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
-    NiceMock<MockLed> led;
-    EXPECT_CALL(led, getTrigger()).WillOnce(Return("timer"));
-    EXPECT_CALL(led, getDelayOn()).WillOnce(Return(500));
-    EXPECT_CALL(led, getDelayOff()).WillOnce(Return(500));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    EXPECT_CALL(*led, getTrigger()).WillOnce(Return("timer"));
+    EXPECT_CALL(*led, getDelayOn()).WillOnce(Return(500));
+    EXPECT_CALL(*led, getDelayOff()).WillOnce(Return(500));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     EXPECT_EQ(phy.state(), Action::Off);
 }
 
 TEST(Physical, off)
 {
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
-    NiceMock<MockLed> led;
-    ON_CALL(led, getMaxBrightness()).WillByDefault(Return(127));
-    EXPECT_CALL(led, getTrigger()).WillOnce(Return("none"));
-    EXPECT_CALL(led, getBrightness()).WillOnce(Return(phosphor::led::DEASSERT));
-    EXPECT_CALL(led, setBrightness(phosphor::led::DEASSERT)).Times(0);
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    ON_CALL(*led, getMaxBrightness()).WillByDefault(Return(127));
+    EXPECT_CALL(*led, getTrigger()).WillOnce(Return("none"));
+    EXPECT_CALL(*led, getBrightness())
+        .WillOnce(Return(phosphor::led::DEASSERT));
+    EXPECT_CALL(*led, setBrightness(phosphor::led::DEASSERT)).Times(0);
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     phy.state(Action::Off);
     EXPECT_EQ(phy.state(), Action::Off);
 }
@@ -157,12 +158,12 @@ TEST(Physical, on)
     constexpr unsigned long asserted = 127;
 
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
-    NiceMock<MockLed> led;
-    ON_CALL(led, getMaxBrightness()).WillByDefault(Return(asserted));
-    EXPECT_CALL(led, getTrigger()).WillOnce(Return("none"));
-    EXPECT_CALL(led, setTrigger("none"));
-    EXPECT_CALL(led, setBrightness(asserted));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    ON_CALL(*led, getMaxBrightness()).WillByDefault(Return(asserted));
+    EXPECT_CALL(*led, getTrigger()).WillOnce(Return("none"));
+    EXPECT_CALL(*led, setTrigger("none"));
+    EXPECT_CALL(*led, setBrightness(asserted));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     phy.state(Action::On);
     EXPECT_EQ(phy.state(), Action::On);
 }
@@ -170,12 +171,12 @@ TEST(Physical, on)
 TEST(Physical, blink)
 {
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
-    NiceMock<MockLed> led;
-    EXPECT_CALL(led, getTrigger()).WillOnce(Return("none"));
-    EXPECT_CALL(led, setTrigger("timer"));
-    EXPECT_CALL(led, setDelayOn(500));
-    EXPECT_CALL(led, setDelayOff(500));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    EXPECT_CALL(*led, getTrigger()).WillOnce(Return("none"));
+    EXPECT_CALL(*led, setTrigger("timer"));
+    EXPECT_CALL(*led, setDelayOn(500));
+    EXPECT_CALL(*led, setDelayOff(500));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     phy.state(Action::Blink);
     EXPECT_EQ(phy.state(), Action::Blink);
 }
@@ -183,10 +184,10 @@ TEST(Physical, blink)
 TEST(Physical, ctor_none_trigger_asserted_brightness)
 {
     sdbusplus::bus_t bus = sdbusplus::bus::new_default();
-    NiceMock<MockLed> led;
-    EXPECT_CALL(led, getTrigger()).WillRepeatedly(Return("none"));
-    EXPECT_CALL(led, getBrightness()).WillRepeatedly(Return(127));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    EXPECT_CALL(*led, getTrigger()).WillRepeatedly(Return("none"));
+    EXPECT_CALL(*led, getBrightness()).WillRepeatedly(Return(127));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     EXPECT_EQ(phy.state(), Action::Off);
 }
 
@@ -196,14 +197,14 @@ TEST(Physical, on_to_off)
     constexpr unsigned long asserted = 127;
 
     auto bus = sdbusplus::bus::new_default();
-    NiceMock<MockLed> led;
-    ON_CALL(led, getMaxBrightness()).WillByDefault(Return(asserted));
-    EXPECT_CALL(led, getTrigger()).Times(1).WillOnce(Return("none"));
+    auto led = std::make_unique<NiceMock<MockLed>>();
+    ON_CALL(*led, getMaxBrightness()).WillByDefault(Return(asserted));
+    EXPECT_CALL(*led, getTrigger()).Times(1).WillOnce(Return("none"));
     constexpr auto deasserted = phosphor::led::DEASSERT;
-    EXPECT_CALL(led, getBrightness()).WillOnce(Return(deasserted));
-    EXPECT_CALL(led, setBrightness(asserted));
-    EXPECT_CALL(led, setBrightness(deasserted));
-    phosphor::led::Physical phy(bus, LED_OBJ, led);
+    EXPECT_CALL(*led, getBrightness()).WillOnce(Return(deasserted));
+    EXPECT_CALL(*led, setBrightness(asserted));
+    EXPECT_CALL(*led, setBrightness(deasserted));
+    phosphor::led::Physical phy(bus, LED_OBJ, std::move(led));
     phy.state(Action::On);
     EXPECT_EQ(phy.state(), Action::On);
     phy.state(Action::Off);
