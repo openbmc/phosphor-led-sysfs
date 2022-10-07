f661782763c412d38f0b629df3ec91b12e196560
Jayashree Dhanapal <jayashree-d@hcl.com>
2022-10-07 13:34:16 +0530

    Support a executable for LED DBUS API method
    Added a new executable for LED DBUS API method to communicate
    between udev and application.
    
    Executable will call Dbus API method in the phosphor-led-sysfs
    repository to pass LED name as argument from udev, after the
    primary service started.
    
    Tested: Tested the executable to be copied in the destination
    path in Facebook YosemiteV2 platform.
    
    Signed-off-by: Jayashree Dhanapal <jayashree-d@hcl.com>
    Change-Id: I14952d71d86b570870f700c4e639d9cd0b45010e

---
+++ add_led_action.cpp
@@ -0,0 +1,73 @@
+#include "argument.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/bus.hpp>
+
+constexpr auto ledConnection = "xyz.openbmc_project.LED.Controller";
+constexpr auto ledPath = "/xyz/openbmc_project/led";
+constexpr auto ledPresenceInterface = "xyz.openbmc_project.Led.Sysfs.Internal";
+constexpr auto ledAddMethod = "AddLed";
+constexpr auto ledClassRoot = "/sys/class/leds/";
+
+std::string rootPathVerify(std::string path)
+{
+    int pos = 0;
+    int index = path.find(ledClassRoot, pos);
+
+    if (index != 0)
+    {
+        lg2::error("Invalid sys path - {PATH}", "PATH", path);
+        throw std::invalid_argument("Invalid argument");
+    }
+    std::string led = path.substr(strlen(ledClassRoot));
+    return led;
+}
+
+void addLed(std::string ledName)
+{
+    try
+    {
+        auto bus = sdbusplus::bus::new_default();
+        auto method = bus.new_method_call(ledConnection, ledPath,
+                                          ledPresenceInterface, ledAddMethod);
+        method.append(ledName);
+        bus.call(method);
+    }
+    catch (const std::exception& e)
+    {
+        lg2::error("Unable to add LED name - {LEDNAME}", "LEDNAME", ledName);
+        throw e;
+    }
+}
+
+/* Each LED udev event will trigger systemd service (sysfs-led@.service)
+ * Systemd service will invoke the binary (add-led-action) by passing LED
+ * name as argument.
+ *
+ * Usage: /usr/libexec/phosphor-led-sysfs/add-led-action [options]
+ * Options:
+ *  --help           Print this menu
+ *  --path=<path>    absolute path of LED in sysfs; like /sys/class/leds/<name>
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+    // Read arguments.
+    auto options = phosphor::led::ArgumentParser(argc, argv);
+
+    // Parse out Path argument.
+    auto path = std::move((options)["path"]);
+    if (path == phosphor::led::ArgumentParser::empty_string)
+    {
+        phosphor::led::ArgumentParser::usage(argv);
+        lg2::error("Argument parser error : Path not specified");
+        throw std::invalid_argument("Invalid argument");
+    }
+
+    std::string name = rootPathVerify(path);
+
+    addLed(name);
+
+    return 0;
+}
